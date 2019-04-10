a97457e2ac9a086b8fb2d56d20f6ae8f0d20c352
Lawrence <lawbraun.almeida@gmail.com>
2019-04-10 13:55:10 +0100

    make nav search visible

--- components/navigation/Navigation.vue
+++ components/navigation/Navigation.vue
@@ -39,7 +39,7 @@
         </div>
 
         <div v-if="showDashboardActions" class="d-flex push-left-auto">
-          <!--div
+          <div
             :class="{ 'has-addons has-query': hasQueryTerm }"
             class="field search-field"
           >
@@ -62,7 +62,7 @@
                 <Icon :icon="'close-circle-outline'" />
               </a>
             </div>
-          </div-->
+          </div>
 
           <!--div class="field has-addons">
             <div class="control">
